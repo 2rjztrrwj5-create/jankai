e50c856403ab42a6dd3783ba385449a75fa230e2
Ubuntu <ubuntu@ip-172-31-43-161.ap-northeast-1.compute.internal>
2026-09-06 03:36:00 +0000

    add minimum password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, length: { minimum: 6 }, allow_nil: true
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
