e856cdb8b77c95e5afb65fab2bcc4369ae6ddef7
Ubuntu <ubuntu@ip-172-31-43-161.ap-northeast-1.compute.internal>
2026-09-15 07:42:40 +0000

    add japanese error messages and fix missing email_address validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+  validates :email_address, presence: true, uniqueness: true
 
   validates :name, presence: true
   has_many :posts, dependent: :destroy
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Jankai
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_support.use_solid_cache = false
   end
