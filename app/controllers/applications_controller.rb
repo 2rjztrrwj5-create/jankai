f3f30f1cbfe4ca3e64a543495979ce7de924050e
Ubuntu <ubuntu@ip-172-31-43-161.ap-northeast-1.compute.internal>
2026-09-22 08:13:32 +0000

    implement group feature with application create/approve/reject

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationsController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
-    @application = @group.applications.new(user: Current.user, status: :pending)
-    @application.save
-    redirect_to @group
+    if @group.applications.exists?(user: Current.user) || @group.users.include?(Current.user)
+      redirect_to @group, alert: "既に申請済み、または参加済みです。"
+    else
+      @application = @group.applications.new(user: Current.user, status: :pending)
+      @application.save
+      redirect_to @group, notice: "参加申請を送信しました。"
+    end
   end
 end
