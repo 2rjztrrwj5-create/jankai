4691ec2e30d6f436e898cde8fca4fd9d4a8ee017
Ubuntu <ubuntu@ip-172-31-43-161.ap-northeast-1.compute.internal>
2026-09-24 11:16:41 +0000

    make prefecture optional when format is online

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   validates :body, presence: true
   validates :event_at, presence: true
   validates :capacity, presence: true, numericality: { greater_than_or_equal_to: 1 }
-  validates :prefecture, presence: true
+  validates :prefecture, presence: true, if: -> { format == "offline" }
   validates :prefecture, format: { without: /[a-zA-Z]/, message: "にはアルファベットを含めないでください" }
   enum :format, { offline: 0, online: 1 }
   has_many :comments, dependent: :destroy
